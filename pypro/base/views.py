16feb6d7ef1ac9174b468799bbb08b93095e5185
adrianodz <adriano.castan@gmail.com>
2020-11-20 21:24:34 -0300

    Test Sentry

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    # raise ValueError('test error..')
+    raise ValueError('test error..')
     return HttpResponse('<html><body>Hola Django</body></html>')
